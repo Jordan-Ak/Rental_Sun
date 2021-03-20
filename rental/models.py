e76580f99a06477ed226f729c3526675ac1b9346
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-03-20 21:28:39 +0100

    Added abstract model to models.

--- rental/models.py
+++ rental/models.py
@@ -1,15 +1,29 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 
-class Friend(models.Model):
+class OwnedModel(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
+
+    class Meta:
+        abstract = True
+
+class Friend(OwnedModel):
     name = models.CharField(max_length = 100)
 
-class Belonging(models.Model):
+class Belonging(OwnedModel):
     name = models.CharField(max_length = 100)
 
-class Borrowed(models.Model):
+class Borrowed(OwnedModel):
     what = models.ForeignKey(Belonging, on_delete = models.CASCADE)
     to_who = models.ForeignKey(Friend, on_delete = models.CASCADE)
     when = models.DateTimeField(auto_now_add = True)
     returned = models.DateTimeField(null = True, default = '')
+
+
+
+
